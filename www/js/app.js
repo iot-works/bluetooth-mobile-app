b1dfdb78c6c9a833407e928b40c45bafe5728e7d
Fengda HUANG <h@phodal.com>
2015-07-14 09:13:23 +0800

    create ble services

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ble.services'])
 
   .run(function($ionicPlatform, $cordovaBluetoothSerial) {
     $ionicPlatform.ready(function() {
